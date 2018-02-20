9dfa9f918b933fab52e42767233f0b699a18ce22
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-02-20 17:58:10 +0300

    Adding props to sidebar component

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -3,22 +3,35 @@ import React from 'react';
 import AppLayout from '../components/AppLayout';
 import Channels from '../components/Channels';
 import Header from '../components/Header';
-import Input from '../components/Input';
+import SendMessage from '../components/SendMessage';
 import Messages from '../components/Messages';
 import Teams from '../components/Teams';
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
+    <Teams teams={[
+        { id: 1, letter: 'A' },
+        { id: 2, letter: 'B' },
+      ]}
+    />
+    <Channels
+      teamName="Team Name"
+      userName="User Name"
+      channels={[
+        { id: 1, name: 'general' },
+        { id: 2, name: 'random' },
+      ]}
+      users={[
+        { id: 1, name: 'slackbot' },
+        { id: 2, name: 'user1' },
+      ]}
+    />
+    <Header channelName="general" />
     <Messages>
       <ul className="message-list">
         <li>Item</li>
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="" />
-    </Input>
+    <SendMessage channelName="general" />
   </AppLayout>
 );
