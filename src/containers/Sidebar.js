b7ac8a01ee69986961b9ed7f0d0e92aaf433b722
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-02-21 17:05:20 +0300

    Cleaning up

---
+++ src/containers/Sidebar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import decode from 'jwt-decode';
+
+import Teams from '../components/Teams';
+import Channels from '../components/Channels';
+
+const Sidebar = ({ data: { loading, allTeams }, currentTeamId }) => {
+  if (loading) return null;
+
+  const currentTeamIdx = _.findIndex(allTeams, ['id', currentTeamId]);
+  const currentTeam = allTeams[currentTeamIdx];
+  let username = '';
+  try {
+    const token = localStorage.getItem('token');
+    const { user } = decode(token);
+    // eslint-disable-next-line prefer-destructuring
+    username = user.username;
+  } catch (err) {
+    username = '';
+  }
+
+  return [
+    <Teams
+      key="team-sidebar"
+      teams={allTeams.map(team => ({
+        id: team.id,
+        letter: team.name.charAt(0).toUpperCase(),
+      }))}
+    />,
+    <Channels
+      key="channels-sidebar"
+      teamName={currentTeam.name}
+      username={username}
+      channels={currentTeam.channels}
+      users={[
+        { id: 1, name: 'slackbot' },
+        { id: 2, name: 'user1' },
+      ]}
+    />,
+  ];
+};
+
+Sidebar.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.object.isRequired,
+};
+
+const allTeamsQuery = graphql(gql`
+  query {
+    allTeams {
+      id
+      name
+      channels {
+        id
+        name
+      }
+    }
+  }`);
+
+// Enhance our component.
+const SidebarWithData = allTeamsQuery(Sidebar);
+
+// Export the enhanced component.
+export default SidebarWithData;
--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,31 +1,14 @@
 import React from 'react';
 
 import AppLayout from '../components/AppLayout';
-import Channels from '../components/Channels';
 import Header from '../components/Header';
 import SendMessage from '../components/SendMessage';
 import Messages from '../components/Messages';
-import Teams from '../components/Teams';
+import Sidebar from '../containers/Sidebar';
 
 export default () => (
   <AppLayout>
-    <Teams teams={[
-        { id: 1, letter: 'A' },
-        { id: 2, letter: 'B' },
-      ]}
-    />
-    <Channels
-      teamName="Team Name"
-      userName="User Name"
-      channels={[
-        { id: 1, name: 'general' },
-        { id: 2, name: 'random' },
-      ]}
-      users={[
-        { id: 1, name: 'slackbot' },
-        { id: 2, name: 'user1' },
-      ]}
-    />
+    <Sidebar currentTeamId={1} />
     <Header channelName="general" />
     <Messages>
       <ul className="message-list">
