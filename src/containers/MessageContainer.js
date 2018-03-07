867eb51a82dead2acb1e6b313db95581f9c65032
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-03-07 15:19:29 +0300

    Cleaning up

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
@@ -6,29 +6,62 @@ import { Comment } from 'semantic-ui-react';
 
 import Messages from '../components/Messages';
 
-const MessageContainer = ({ data: { loading, messages }, channelId }) => {
-  if (loading) return null;
-  return (
-    <Messages>
-      <Comment.Group>
-        {messages.map(message => (
-          <Comment key={`${message.id}-message`}>
-            <Comment.Content>
-              <Comment.Author as="a">{message.user.username}</Comment.Author>
-              <Comment.Metadata>
-                <div>{message.created_at}</div>
-              </Comment.Metadata>
-              <Comment.Text>{message.text}</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-        ))}
-      </Comment.Group>
-    </Messages>
-  );
-};
+const newChannelMessageSubscription = gql`
+  subscription($channelId: Int!) {
+    newChannelMessage(channelId: $channelId) {
+      id
+      text
+      user {
+        username
+      }
+      created_at
+    }
+  }
+`;
+
+class MessageContainer extends Component {
+  componentWillMount() {
+    this.props.data.subscribeToMore({
+      document: newChannelMessageSubscription,
+      variables: {
+        channelId: this.props.channelId,
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) {
+          return prev;
+        }
+        return {
+          ...prev,
+          messages: [...prev.messages, subscriptionData.newChannelMessage],
+        };
+      },
+    });
+  }
+  render() {
+    const { data: { loading, messages } } = this.props;
+    if (loading) return null;
+    return (
+      <Messages>
+        <Comment.Group>
+          {messages.map(message => (
+            <Comment key={`${message.id}-message`}>
+              <Comment.Content>
+                <Comment.Author as="a">{message.user.username}</Comment.Author>
+                <Comment.Metadata>
+                  <div>{message.created_at}</div>
+                </Comment.Metadata>
+                <Comment.Text>{message.text}</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </Messages>
+    );
+  }
+}
 
 MessageContainer.propTypes = {
   channelId: PropTypes.number.isRequired,
